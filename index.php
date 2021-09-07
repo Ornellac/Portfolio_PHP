2ccd1afc1aa515cba0adc108b30835afcee47ab2
ornella <cuttaia.ornella@gmail.com>
2021-09-07 17:04:00 +0200

    Portfolio Version PHP

---
+++ index.php
@@ -0,0 +1,121 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Patrick+Hand+SC&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Alumni+Sans&display=swap" rel="stylesheet">
+    <link rel="icon" type="image/x-icon" href="./img/logo.gif">
+    <link type="text/css" rel="stylesheet" href="./CSS/index.css">
+    <title>Ornella Cuttaia Portfolio</title>
+</head>
+<header>
+    <nav>
+        <ul>
+            <li class="deroulant"><a href='#p1'>A PROPOS</a>
+                <ul class="sous">
+                    <li><a href='#p1'>QUI JE SUIS</a></li>
+                    <li><a href='#p2'>MES ETUDES</a></li>
+                    <li><a href='#p3'>MES PROJETS</a></li>
+                </ul>
+            </li>
+            <li class="deroulant"><a href='#p4'>MES FORMATIONS</a>
+                <ul class="sous">
+                    <li><a href='#p4'>METIER DE L'INFORMATIQUE</a></li>
+                    <li><a href='#p5'>DEVELOPPEUSE BACK END ORIENTE DATA</a></li>
+                </ul>
+            </li>
+            <li><a href='#p6'>ME CONTACTER</a></li>
+        </ul>
+        <img class="footernavimg" src="./img/nav_1.bmp"/>
+    </nav>
+</header>
+
+<body>
+    <img class="sep" id="p1" src='./img/separation_.bmp'>
+    <div class="page1">
+        <a href="#"><img class="donut" src="img/donut1.gif" onmouseover="this.src='img/donut_hover.gif'" onmouseout="this.src='img/donut1.gif'"></a> <span style="font-size:14px;">←</span> <span style="font-size:16px;">Appuis sur le donut pour revenir au menu de navigation.</span> <br>
+        <h1 class="titre"><span style="color: rgb(152, 141, 172);">Q</span>ui je suis ?</h1>
+        <p>Je m'appelle <span style="color: rgb(143, 134, 160);">Ornella</span> et j'ai 27 ans.<br> Je suis née le <span style="color: rgb(143, 134, 160);">16 juillet 1994</span> à Charleroi en Belgique.</p>
+        <img class="com" src="./img/coming_soon_White.gif">
+    </div>
+
+    <img class="sep" id="p2" src='./img/separation_grise.bmp'>
+    <div class="page2">
+        <a href="#"><img class="donut" src="img/donut2.gif" onmouseover="this.src='img/donut_hover.gif'" onmouseout="this.src='img/donut2.gif'"></a> <span style="font-size:14px;">←</span> <span style="font-size:16px;">Appuis sur le donut pour revenir au menu de navigation.</span> <br>
+        <h1 class="titre"><span style="color: rgb(194, 127, 147);">M</span>es études</h1>
+        <img class="com" src="./img/coming_soon_Grey.gif">    
+    </div>
+
+    <img class="sep" id="p3" src='./img/separation_grise_to_.bmp'>
+    <div class="page3">
+        <a href="#"><img class="donut" src="img/donut3.gif" onmouseover="this.src='img/donut_hover.gif'" onmouseout="this.src='img/donut3.gif'"></a> <span style="font-size:14px;">←</span> <span style="font-size:16px;">Appuis sur le donut pour revenir au menu de navigation.</span> <br>
+        <h1 class="titre"><span style="color: rgb(107, 156, 118);">M</span>es projets</h1>
+        <img class="com" src="./img/coming_soon_White.gif">
+    </div>
+
+    <img class="sep" id="p4" src='./img/separation_grise.bmp'>
+    <div class="page4">
+        <a href="#"><img class="donut" src="img/donut4.gif" onmouseover="this.src='img/donut_hover.gif'" onmouseout="this.src='img/donut4.gif'"></a> <span style="font-size:14px;">←</span> <span style="font-size:16px;">Appuis sur le donut pour revenir au menu de navigation.</span> <br>
+        <h1 class="titre"><span style="color: rgb(167, 155, 0);">D</span>écouverte des métiers de l'informatiques</h1>
+        <img class="com" src="./img/coming_soon_Grey.gif">
+    </div>
+
+    <img class="sep" id="p5" src='./img/separation_grise_to_.bmp'>
+    <div class="page5">
+        <a href="#"><img class="donut" src="img/donut6.gif" onmouseover="this.src='img/donut_hover.gif'" onmouseout="this.src='img/donut6.gif'"></a> <span style="font-size:14px;">←</span> <span style="font-size:16px;">Appuis sur le donut pour revenir au menu de navigation.</span> <br>
+        <h1 class="titre"><span style="color: rgb(47, 121, 102);">D</span>évelloppeuse <span style="color: rgb(47, 121, 102);">BACK-END</span> / <span style="color: rgb(47, 121, 102);">FRONT-END</span> orientée <span style="color: rgb(47, 121, 102);">DATA</span></h1>
+        <img class="com" src="./img/coming_soon_White.gif">
+    </div>
+
+    <img class="sep" id="p6" src='./img/separation_grise.bmp'>
+    <div class="page6">
+        <a href="#"><img class="donut" src="img/donut1.gif" onmouseover="this.src='img/donut_hover.gif'" onmouseout="this.src='img/donut1.gif'"></a> <span style="font-size:14px;">←</span> <span style="font-size:16px;">Appuis sur le donut pour revenir au menu de navigation.</span> <br>
+        <h1 class="titre"><span style="color: rgb(152, 141, 172);">C</span>ontactez moi</h1>
+        <form method="post">
+        <label>Email</label>
+        <input type="email" name="email" required><br>
+        <label>Message</label>
+        <textarea name="message" required></textarea><br>
+        <input type="submit">
+    </form>
+    <?php
+    if (isset($_POST['message'])) {
+        $position_arobase = strpos($_POST['email'], '@');
+        if ($position_arobase === false)
+            echo '<p>Votre email doit comporter un arobase.</p>';
+        else {
+            $retour = mail('cuttaia.ornella@gmail.com', 'Envoi depuis la page Contact', $_POST['message'], 'From: ' . $_POST['email']);
+            if($retour)
+                echo '<p>Votre message a été envoyé.</p>';
+            else
+                echo '<p>Erreur.</p>';
+        }
+    }
+    ?>
+
+    <img class="com" src="./img/coming_soon_Grey.gif">
+    </div> 
+
+    <img class="sep" src='./img/separation_grise_to_.bmp'> <!-- REFAIRE CAR DEPEND DU Nbre DE SECTION SI FINI GRIS OU SI FINI BLANC ! -->
+
+</body>
+
+<footer>
+    <div class="footerContenant">
+        <img class="footernavimg" src="img/footer_.bmp">
+        <div class="textfooter"> Create by <br> © Ornella Cuttaia<br> <br>Make with <span style="color: rgb(204, 189, 230);">love</span> and <span style="color: rgb(204, 189, 230);">passion</span> 🖤 </div>
+    </div>
+</footer>
+
+</html>
